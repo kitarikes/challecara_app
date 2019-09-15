74fdd30203b4ade9a7aa2e8962586627a8a63c11
kitarikes <ryosuke092413@gmail.com>
2019-09-16 04:26:59 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+
+  
+  root 'users/sessions#new'
+end
