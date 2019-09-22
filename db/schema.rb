02404becd0bef44f10e79d7fb70528401a7fa8eb
kitarikes <ryosuke092413@gmail.com>
2019-09-23 01:42:22 +0900

    add-model Subject

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_07_072657) do
+ActiveRecord::Schema.define(version: 2019_09_22_162428) do
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
