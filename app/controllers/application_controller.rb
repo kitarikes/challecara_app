cf53350b9eeab7828da17c2dc941343dd8183977
kitarikes <ryosuke092413@gmail.com>
2019-09-24 09:31:38 +0900

    Revert "ログアウトボタン作成"
    This reverts commit 1002927ddb7df72dcb2b0548087f8baf037aec4e.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource)
-    "/home/top" # ログアウト後に遷移するpathを設定
+    "home/top" # ログアウト後に遷移するpathを設定
   end
 
 end
