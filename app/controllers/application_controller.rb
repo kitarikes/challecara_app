686d848b2575ec70536f00c807fe21b097ada909
kitarikes <ryosuke092413@gmail.com>
2019-11-11 17:46:43 +0900

    11/11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   
   before_action :authenticate_user!
   def after_sign_in_path_for(resource)
-    "/subjects/top"  # ログイン後に遷移するpathを設定
+    "user_path(resource)"  # ログイン後に遷移するpathを設定
   end
 
   def after_sign_out_path_for(resource)
